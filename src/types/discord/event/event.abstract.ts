b796e8eccd4018b2e6829673427ce361425d16ad
Elkost <elkost@MacBook-Pro-Elkost.local>
2023-09-27 03:41:25 +0300

    refactor: "execute" method in Event no more abstract and throw error

--- src/types/discord/event/event.abstract.ts
+++ src/types/discord/event/event.abstract.ts
@@ -1,7 +1,10 @@
 import type { ClientEvents } from 'discord.js';
 
+import { AbstractClassError } from '#lib/index.js';
 import type { IEvent } from '#types/index.js';
 
 export abstract class AEvent<K extends keyof ClientEvents> implements IEvent<K>{
-	abstract execute(...args: ClientEvents[K]): Promise<void>;
+	async execute (..._args: ClientEvents[K]): Promise<void> {
+		throw new AbstractClassError('Abstract method not implemented');
+	}
 }
